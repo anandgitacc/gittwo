14e7da0d4cd3f3379c383ec210f04ef5dda5dd88
anandgitacc <anand.akmail@gmail.com>
2021-09-30 18:40:28 +0530

    Sample one

---
+++ src/org/testtwo/SampleTwo.java
@@ -0,0 +1,27 @@
+package org.testtwo;
+
+public class SampleTwo {
+	
+	public void empName() {
+		System.out.println("Employee name is Anand");
+	}
+	
+	public void empMobno() {
+		System.out.println("Employee mobile no is 8667070832");
+	}
+	
+	public void empAdd() {
+		System.out.println("Employee Add is Chennai");
+	}
+	
+	public static void main(String[] args) {
+		
+		SampleTwo a = new SampleTwo();
+		
+		a.empName();
+		a.empMobno();
+		a.empAdd();
+		
+	}
+
+}
